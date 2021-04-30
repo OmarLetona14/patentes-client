172b1739c8bd85bf9d392272755ea12aaf34f2dc
Omar Letona <omarletonaf@gmail.com>
2021-04-30 12:40:09 -0600

    Added remote api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  api_host: 'http://104.154.244.175:3000',
+  firebaseConfig: {
+    apiKey: "AIzaSyDw0sPPglrN201h-a5BdU5GaQRS3YfZGFs",
+    authDomain: "pantent-project.firebaseapp.com",
+    projectId: "pantent-project",
+    storageBucket: "pantent-project.appspot.com",
+    messagingSenderId: "296660696171",
+    appId: "1:296660696171:web:60daa53ab54157d459c52e",
+    measurementId: "G-CL5YRPNQ2D"
+  }
 };
